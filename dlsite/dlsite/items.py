c943647047ca8c0cee0fa9a308c838c9691948f8
mudssky <mudssky@gmail.com>
2023-08-01 23:00:51 +0800

    ✨ feat: add dlsite spider

---
+++ dlsite/dlsite/items.py
@@ -0,0 +1,42 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class DlsiteItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    # サークル名
+    # circle_name=scrapy.Field()
+    # cv_list=scrapy.Field()
+    # 年齢指定
+    # age_judge=scrapy.Field()
+
+    # 販売日
+    on_sale = scrapy.Field()
+    # 年齢指定
+    age_judge = scrapy.Field()
+    #    サークル名
+    circle_name = scrapy.Field()
+    # ブランド名
+    brand_name = scrapy.Field()
+    maker_id = scrapy.Field()
+    # 作品形式
+    work_genre = scrapy.Field()
+
+    # ファイル形式	MP3/ wav同梱
+    file_type = scrapy.Field()
+    # ジャンル
+    genre = scrapy.Field()
+    # ファイル容量
+    file_capcity = scrapy.Field()
+
+    cover_url = scrapy.Field()
+    pass
+
+
+class DoujinItem(DlsiteItem):
+    cv_list = scrapy.Field()
---
+++ dlsite/dlsite/spiders/rjid.py
@@ -0,0 +1,45 @@
+import scrapy
+from scrapy.exceptions import CloseSpider
+
+
+class rjidSpider(scrapy.Spider):
+    name = 'rjid'
+    allowed_domains = ['www.dlsite.com']
+
+    def __init__(self, start_id=1, end_id=None, *args, **kwargs):
+        # 调用父类的构造函数
+        super(rjidSpider, self).__init__(*args, **kwargs)
+
+        self.start_id = int(start_id)
+        if end_id is None:
+            self.end_id = self.start_id + 1
+        else:
+            self.end_id = int(end_id)
+
+        self.logger.debug(f'start_id: {start_id} ,end_id: {end_id}')
+        self.validate_argument()
+        # 分成两段，6位id时期和8位id时期。
+        # RJ001016 RJ441531
+        # RJ01000101 - ？
+
+    def get_rjid_list(self):
+        for num in range(self.start_id, self.end_id):
+            if num >= 1 and num <= 441531:
+                num_str = f'RJ{num:0>6}'
+                yield num_str
+            elif num >= 1000101:
+                num_str = f'RJ{num:0>8}'
+                yield num_str
+
+    def validate_argument(self):
+        if self.start_id == 1 and self.end_id == 1:
+            raise CloseSpider('No start_id,end_id specified')
+
+    def start_requests(self):
+        for rjid in self.get_rjid_list():
+            getchu_url = f'https://www.dlsite.com/maniax/work/=/product_id/{rjid}.html'
+            metaDict = {"rjid": rjid}
+            yield scrapy.Request(getchu_url, callback=self.parse, meta=metaDict)
+
+    def parse(self, response):
+        pass
