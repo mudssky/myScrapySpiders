06ed79f2de704cdbf48ab8229b1fe30fe62b4a89
mudssky <mudssky@gmail.com>
2023-08-07 01:46:03 +0800

    ✨ feat: add filter

--- getchu/getchu/spiders/getchuID.py
+++ getchu/getchu/spiders/getchuID.py
@@ -45,28 +45,37 @@ class getchuIDSpider(scrapy.Spider):
         """
 
         def wrapper(self, *args, **kwargs):
-            # print(self.value)  # 使用类的属性value
-            # print('test_decorator')
             input_ids = func(self, *args, **kwargs)
             db_existing_id_set = {x['getchu_id'] for x in self.collection.find({})}
-            db_not_exist_id = set(input_ids) - db_existing_id_set
-            for id in db_not_exist_id:
-                yield id
+            for id in input_ids:
+                if id not in db_existing_id_set:
+                    yield id
 
         return wrapper
 
-    @not_in_db_id_filter
+    def unknown_id_filter(func):
+        """过滤出unknown项目的id
+
+        Args:
+            func (_type_): _description_
+        """
+
+        def wrapper(self, *args, **kwargs):
+            input_ids = func(self, *args, **kwargs)
+            unknown_ids = {x['getchu_id'] for x in self.collection.find({'tab_type': 'unknown'})}
+            for id in input_ids:
+                if id in unknown_ids:
+                    yield id
+
+        return wrapper
+
+    # @unknown_id_filter
     def get_ids(self):
         for id in range(self.start_id, self.end_id):
             yield id
 
     def start_requests(self):
         self.connect_mongodb()
-        ids = self.get_ids()
-        id_list = list(ids)
-        print(f'ids:{id_list}')
-        print(len(id_list))
-        raise CloseSpider('test dec')
         for id in self.get_ids():
             getchu_url = r'https://www.getchu.com/soft.phtml?id=' + str(id) + '&gc=gc'
             metaDict = {"getchu_id": id}
@@ -89,71 +98,44 @@ class getchuIDSpider(scrapy.Spider):
             '/goods/adult.html': 'adult_goods',
             '/top.html': "top",
         }
-
+        unknown_tab_type = 'unknown'
+        soft_table = response.xpath('//table[@id="soft_table"]')
+        if soft_table:
+            unknown_tab_type = 'other'
         # self.logger.debug(f'tab_link:{tab_link} ')
         # self.logger.debug(tab_link in tab_type_dict)
-        return tab_type_dict.get(tab_link) or 'unknown'
+        return tab_type_dict.get(tab_link) or unknown_tab_type
 
     def extract_common(self, response, loader=None):
         common_dict = {}
         common_dict['getchu_id'] = response.meta['getchu_id']
         common_dict['title'] = response.css('h1#soft-title::text').get().strip()
         soft_table = response.xpath('//table[@id="soft_table"]')
-        price = soft_table.xpath(
-            '//td[contains(text(),"定価")]/following-sibling::td/text()'
-        ).get()
+        price = soft_table.xpath('//td[contains(text(),"定価")]/following-sibling::td/text()').get()
         common_dict['price'] = price
         common_dict['price_taxed'] = price
-        common_dict['midea'] = soft_table.xpath(
-            '//td[contains(text(),"メディア")]/following-sibling::td/text()'
-        ).get()
-        common_dict['on_sale'] = soft_table.xpath(
-            '//td[contains(text(),"発売日")]/following-sibling::td/a/text()'
-        ).get()
-        common_dict['jan_code'] = soft_table.xpath(
-            '//td[contains(text(),"JANコード")]/following-sibling::td/text()'
-        ).get()
-        common_dict['is_r18'] = soft_table.xpath(
-            '//span[contains(@class,"redb") and contains(text(),"18歳未満の方は購入できません")] = true()'
-        ).get()
+        common_dict['midea'] = soft_table.xpath('//td[contains(text(),"メディア")]/following-sibling::td/text()').get()
+        common_dict['on_sale'] = soft_table.xpath('//td[contains(text(),"発売日")]/following-sibling::td/a/text()').get()
+        common_dict['jan_code'] = soft_table.xpath('//td[contains(text(),"JANコード")]/following-sibling::td/text()').get()
+        common_dict['is_r18'] = soft_table.xpath('//span[contains(@class,"redb") and contains(text(),"18歳未満の方は購入できません")] = true()').get()
 
-        common_dict['cover_url'] = soft_table.xpath(
-            '//tr[1]//a[@class="highslide"]/img/@src'
-        ).get()
-        common_dict['cover_url_hd'] = soft_table.xpath(
-            '//tr[1]//a[@class="highslide"]/@href'
-        ).get()
-        common_dict['sample_img_list'] = response.xpath(
-            '//div[contains(@class,"tabletitle") and contains(text(),"サンプル画像")]/following-sibling::div[1]//a/@href'
-        ).getall()
+        common_dict['cover_url'] = soft_table.xpath('//tr[1]//a[@class="highslide"]/img/@src').get()
+        common_dict['cover_url_hd'] = soft_table.xpath('//tr[1]//a[@class="highslide"]/@href').get()
+        common_dict['sample_img_list'] = response.xpath('//div[contains(@class,"tabletitle") and contains(text(),"サンプル画像")]/following-sibling::div[1]//a/@href').getall()
 
-        common_dict['product_code'] = response.xpath(
-            '//td[contains(text(),"品番")]/following-sibling::td/text()'
-        ).get()
+        common_dict['product_code'] = response.xpath('//td[contains(text(),"品番")]/following-sibling::td/text()').get()
         common_dict['tab_type'] = self.tabTypeParse(response)
 
-        common_dict['genre'] = response.xpath(
-            '//td[re:test(text(),"^ジャンル")]/following-sibling::td/text()'
-        ).get()
-        common_dict['subgenre_list'] = response.xpath(
-            '//td[contains(text(),"サブジャンル")]/following-sibling::td/a/text()'
-        ).getall()
+        common_dict['genre'] = response.xpath('//td[re:test(text(),"^ジャンル")]/following-sibling::td/text()').get()
+        common_dict['subgenre_list'] = response.xpath('//td[contains(text(),"サブジャンル")]/following-sibling::td/a/text()').getall()
 
-        common_dict['intro'] = response.xpath(
-            '//div[contains(@class,"tabletitle") and contains(text(),"紹介")]/following-sibling::div[1]//text()'
-        ).getall()
-        brand_a = soft_table.xpath(
-            '//td[contains(text(),"ブランド")]/following-sibling::td/a'
-        )
+        common_dict['intro'] = response.xpath('//div[contains(@class,"tabletitle") and contains(text(),"紹介")]/following-sibling::div[1]//text()').getall()
+        brand_a = soft_table.xpath('//td[contains(text(),"ブランド")]/following-sibling::td/a')
         if len(brand_a) > 1:
             common_dict['brand'] = brand_a.xpath('./text()').get()
-            common_dict['brand_site'] = soft_table.xpath(
-                '//td[contains(text(),"ブランド")]/following-sibling::td/a/@href'
-            ).get()
+            common_dict['brand_site'] = soft_table.xpath('//td[contains(text(),"ブランド")]/following-sibling::td/a/@href').get()
         else:
-            common_dict['brand'] = soft_table.xpath(
-                '//td[contains(text(),"ブランド")]/following-sibling::td/text()'
-            ).get()
+            common_dict['brand'] = soft_table.xpath('//td[contains(text(),"ブランド")]/following-sibling::td/text()').get()
             common_dict['brand_site'] = None
 
         if loader:
@@ -167,9 +149,7 @@ class getchuIDSpider(scrapy.Spider):
     def parse_name_text(self, nametext):
         res = {}
 
-        name_pattern = (
-            r'(?P<name>[^（C]+)?(（(?P<yumi>[^）]+)）)?[^C]*(CV：(?P<cv>[\s\S]+))?'
-        )
+        name_pattern = r'(?P<name>[^（C]+)?(（(?P<yumi>[^）]+)）)?[^C]*(CV：(?P<cv>[\s\S]+))?'
         matched = re.match(name_pattern, nametext, re.S)
         # print(matched.groupdict())
         if not matched:
@@ -183,23 +163,17 @@ class getchuIDSpider(scrapy.Spider):
 
     def parse_chara_list(self, response, loader=None):
         chara_list = []
-        chara_sel_list = response.xpath(
-            '//div[contains(@class,"tabletitle") and contains(text(),"キャラクター")]/following-sibling::table[1]//tr[not(descendant::hr)]'
-        )
+        chara_sel_list = response.xpath('//div[contains(@class,"tabletitle") and contains(text(),"キャラクター")]/following-sibling::table[1]//tr[not(descendant::hr)]')
         for node in chara_sel_list:
             chara_dict = {}
             chara_dict['img'] = node.xpath('./td[1]/img/@src').get()
-            name_text = node.xpath(
-                './td[2]/dl/dt/h2[contains(@class,"chara-name")]/strong//text()'
-            ).getall()
+            name_text = node.xpath('./td[2]/dl/dt/h2[contains(@class,"chara-name")]/strong//text()').getall()
             chara_dict['name_text'] = getchu_item.join_and_strip(name_text)
             name_dict = self.parse_name_text(chara_dict['name_text'])
             chara_dict['yumi'] = name_dict['yumi']
             chara_dict['cv'] = name_dict['cv']
             chara_dict['name'] = name_dict['name']
-            chara_dict['desp'] = getchu_item.join_and_strip(
-                node.xpath('./td[2]/dl/dd//text()').getall()
-            )
+            chara_dict['desp'] = getchu_item.join_and_strip(node.xpath('./td[2]/dl/dd//text()').getall())
             chara_dict['img_whole'] = node.xpath('./td[3]/a/@href').get()
             chara_list.append(chara_dict)
         if loader:
@@ -211,9 +185,7 @@ class getchuIDSpider(scrapy.Spider):
         # self.logger.debug(f'tab_type:{tab_type} ')
 
         if tab_type == 'game' or tab_type == 'dvdpg' or tab_type == 'adult_goods':
-            l = getchu_item.GameItemLoader(
-                item=getchu_item.GameItem(), response=response
-            )
+            l = getchu_item.GameItemLoader(item=getchu_item.GameItem(), response=response)
             #  GetchuItem
             self.extract_common(response=response, loader=l)
             # l.add_value('tab_type', tab_type)
@@ -268,13 +240,9 @@ class getchuIDSpider(scrapy.Spider):
             return l.load_item()
         elif tab_type == 'anime' or tab_type == 'adult_anime':
             if tab_type == 'anime':
-                l = getchu_item.AnimeItemLoader(
-                    item=getchu_item.AnimeItem(), response=response
-                )
+                l = getchu_item.AnimeItemLoader(item=getchu_item.AnimeItem(), response=response)
             else:
-                l = getchu_item.AdultAnimeItemLoader(
-                    item=getchu_item.AdultAnimeItem(), response=response
-                )
+                l = getchu_item.AdultAnimeItemLoader(item=getchu_item.AdultAnimeItem(), response=response)
             self.extract_common(response=response, loader=l)
             soft_table = l.nested_xpath('//table[@id="soft_table"]')
             # soft_table.add_xpath('product_code','//td[contains(text(),"品番")]/following-sibling::td/text()')
@@ -284,33 +252,17 @@ class getchuIDSpider(scrapy.Spider):
                 '//div[contains(@class,"tabletitle") and contains(text(),"スタッフ")]/following-sibling::div[1]//text()',
             )
             return l.load_item()
-        elif (
-            tab_type == 'music'
-            or tab_type == 'goods'
-            or tab_type == 'dakimakura'
-            or tab_type == 'books'
-            or tab_type == 'top'
-        ):
+        elif tab_type == 'music' or tab_type == 'goods' or tab_type == 'dakimakura' or tab_type == 'books' or tab_type == 'top':
             if tab_type == 'music':
-                l = getchu_item.MusicItemLoader(
-                    item=getchu_item.MusicItem(), response=response
-                )
+                l = getchu_item.MusicItemLoader(item=getchu_item.MusicItem(), response=response)
             elif tab_type == 'goods':
-                l = getchu_item.GoodsItemLoader(
-                    item=getchu_item.GoodsItem(), response=response
-                )
+                l = getchu_item.GoodsItemLoader(item=getchu_item.GoodsItem(), response=response)
             elif tab_type == 'dakimakura':
-                l = getchu_item.GoodsItemLoader(
-                    item=getchu_item.GoodsItem(), response=response
-                )
+                l = getchu_item.GoodsItemLoader(item=getchu_item.GoodsItem(), response=response)
             elif tab_type == 'top':
-                l = getchu_item.TopItemLoader(
-                    item=getchu_item.TopItem(), response=response
-                )
+                l = getchu_item.TopItemLoader(item=getchu_item.TopItem(), response=response)
             elif tab_type == 'books':
-                l = getchu_item.BookItemLoader(
-                    item=getchu_item.BookItem(), response=response
-                )
+                l = getchu_item.BookItemLoader(item=getchu_item.BookItem(), response=response)
                 l.add_xpath(
                     'ISBN_13',
                     '//td[contains(text(),"ISBN-13")]/following-sibling::td/text()',
@@ -318,9 +270,7 @@ class getchuIDSpider(scrapy.Spider):
             self.extract_common(response=response, loader=l)
             return l.load_item()
         elif tab_type == 'doujin' or tab_type == 'cosplay':
-            l = getchu_item.DoujinItemLoader(
-                item=getchu_item.DoujinItem(), response=response
-            )
+            l = getchu_item.DoujinItemLoader(item=getchu_item.DoujinItem(), response=response)
             self.extract_common(response=response, loader=l)
             self.parse_chara_list(response=response, loader=l)
             soft_table = l.nested_xpath('//table[@id="soft_table"]')
@@ -358,9 +308,7 @@ class getchuIDSpider(scrapy.Spider):
             return l.load_item()
 
         elif tab_type == 'unknown':
-            l = getchu_item.UnknowItemLoader(
-                item=getchu_item.UnknownItem(), response=response
-            )
+            l = getchu_item.UnknowItemLoader(item=getchu_item.UnknownItem(), response=response)
             l.add_value('tab_type', tab_type)
             l.add_value('getchu_id', response.meta['getchu_id'])
             l.add_value('res_body_size', len(response.body))
