16cc69d540dcc63f0c68775e4c6ef9890d40dd37
mudssky <mudssky@gmail.com>
2023-12-02 14:10:22 +0800

    ✨ feat: update start scripts

--- dlsite/start.ps1
+++ dlsite/start.ps1
@@ -1,7 +1,14 @@
 [CmdletBinding()]
 param(
 	[string]$start_id,
-	[string]$end_id)
-poetry shell
+	[string]$end_id,
+	[switch]$switchEnv
+)
+
+if ($switchEnv) {
+	poetry shell
+	exit 0
+}
+
 
 scrapy crawl rjid -s JOBDIR=.scrapy/jobdir/rjid1  -a start_id=$start_id  -a end_id=$end_id
--- getchu/start.ps1
+++ getchu/start.ps1
@@ -1,9 +1,16 @@
 [CmdletBinding()]
 param(
 	[string]$start_id,
-	[string]$end_id)
+	[string]$end_id,
+	[switch]$switchEnv
+)
 
-poetry shell
+if ($switchEnv) {
+	poetry shell
+	exit 0
+}
+
+
 
 # scrapy crawl getchuID  -s JOBDIR=.scrapy/jobdir/rjid15 -a start_id=1232471  -a end_id=1251084
 scrapy crawl getchuID  -s JOBDIR=.scrapy/jobdir/rjid15 -a start_id=$start_id  -a end_id=$end_id
